ba046da85b3a7155c02801cb95f203a3a122d769
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2023-01-04 13:09:04 -0500

    React router new syntax intro, home single write routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,45 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Write from "./pages/Write";
+import Home from "./pages/Home";
+import Single from "./pages/Single";
+
+
+const router = createBrowserRouter([
+  {
+    // path: "/",
+    // element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
