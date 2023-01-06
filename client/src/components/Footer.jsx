8d58c2ff022592a465ec1e1bbab0a6e70193b2ad
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2023-01-06 13:09:04 -0500

    Navbar and Footer starter files created

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "../img/logo.png";
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer;
