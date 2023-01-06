4f3c81a94c923fa6262515c288b76ad7c4723af5
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2023-01-06 13:09:04 -0500

    import and use Navbar on routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,12 +9,14 @@ import Login from "./pages/Login";
 import Write from "./pages/Write";
 import Home from "./pages/Home";
 import Single from "./pages/Single";
-
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import "./style.scss"
 
 const router = createBrowserRouter([
   {
-    // path: "/",
-    // element: <Layout />,
+    path: "/",
+    element: <Navbar />,
     children: [
       {
         path: "/",
