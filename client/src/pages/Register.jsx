bef9bbcff786ec3549c7982a0a492538ebdca132
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2023-01-12 13:09:04 -0500

    register comp

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [err, setError] = useState(null);
 
-export default Register
+  const navigate = useNavigate();
+
+
+
+  return (
+    <div className="auth">
+      <h1>Register</h1>
+      <form>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+        />
+        <input
+          required
+          type="email"
+          placeholder="email"
+          name="email"
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+        />
+        <button >Register</button>
+        {err && <p>{err}</p>}
+        <span>
+          Do you have an account? <Link to="/login">Login</Link>
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
