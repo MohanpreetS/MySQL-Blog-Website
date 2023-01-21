37a3fdbaaaaef71e6c57d6f746eec3afbdb92606
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2023-01-21 13:09:04 -0500

    navbar component links setup

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -21,7 +21,18 @@ const Navbar = () => {
           <Link className="link" to="/?cat=science">
             <h6>SCIENCE</h6>
           </Link>
-          
+          <Link className="link" to="/?cat=technology">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link className="link" to="/?cat=cinema">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className="link" to="/?cat=design">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className="link" to="/?cat=food">
+            <h6>FOOD</h6>
+          </Link>
           <span>{currentUser?.username}</span>
           {currentUser ? (
             <span onClick={logout}>Logout</span>
