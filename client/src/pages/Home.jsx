afc3d2e47d4cd77987b6589ff78b6c35095aa0c4
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2023-01-23 13:09:04 -0500

    Home component basic design

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import axios from "axios";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [posts, setPosts] = useState([]);
 
-export default Home
+ 
+  return (
+    <div className="home">
+      <div className="posts">
+        {posts.map((post) => (
+          <div className="post" key={post.id}>
+            <div className="img">
+              <img src={`../upload/${post.img}`} alt="" />
+            </div>
+            <div className="content">
+              <Link className="link" to={`/post/${post.id}`}>
+                <h1>{post.title}</h1>
+              </Link>
+              
+              <button>Read More</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
