2399597ac3c46435a6a3c04be6f8d6bb763d75ec
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2023-02-10 13:09:04 -0500

    login - grant jwt

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -33,7 +33,19 @@ export const login = (req, res) => {
     if (err) return res.status(500).json(err);
     if (data.length === 0) return res.status(404).json("User not found!");
 
-   
+    //Check password
+    const isPasswordCorrect = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+
+    if (!isPasswordCorrect)
+      return res.status(400).json("Wrong username or password!");
+
+    const token = jwt.sign({ id: data[0].id }, "jwtkey");
+    const { password, ...other } = data[0];
+
+    
   });
 };
 
